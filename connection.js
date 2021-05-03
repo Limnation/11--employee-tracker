fed3aac35d7018b41d7491aeacbde090ba8133c1
Scott Erwin <scottallenerwin@gmail.com>
2021-05-02 20:23:42 -0400

    added connection and env

--- connection.js
+++ connection.js
@@ -3,3 +3,10 @@
 require("dotenv").config();
 // mySQL required
 const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: process.env.USERNAME_DB,
+  password: process.env.PASSWORD_DB,
+  database: process.env.DATABASE_DB,
+});
