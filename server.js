5af6ddacdd75b24db946a5853cfe2d455b01facd
Scott Erwin <scottallenerwin@gmail.com>
2021-05-03 19:26:37 -0400

    created switch statement

--- server.js
+++ server.js
@@ -1,11 +1,45 @@
 // Includes packages needed for this application
 const inquirer = require("inquirer");
 
-// gets Specific arrays { } from questions.js
-const {} = require("./lib/questions.js");
+// gets Specific arrays { questions } from questions.js
+const { questions } = require("./lib/questions.js");
+
+// arrow fucntion that prompts
+const prompts = () => {
+  inquirer.prompt(questions).then((DBData) => {
+    switch (DBData.DBcases) {
+      case "View All Employees":
+        viewAllEmployees();
+        break;
+      case "View All Employees by Department":
+        viewAllEmployeesByDepartment();
+        break;
+      case "View All Employees by Role":
+        viewAllEmployeesByRole();
+        break;
+      case "Add Employee":
+        addEmployee();
+        break;
+      case "Add Department":
+        addDepartment();
+        break;
+      case "Add Role":
+        addRole();
+        break;
+      case "Update Employee Role":
+        updateEmployeeRole();
+        break;
+      default:
+        endConnection();
+        break;
+    }
+  });
+};
 
 // Creates a function to initialize app
-function init() {}
+function init() {
+  prompts();
+}
 
 // Function call to initialize app
 init();
