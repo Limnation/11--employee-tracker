6d3a084644f295c9786c003d048ef573adec9272
Scott Erwin <scottallenerwin@gmail.com>
2021-05-02 20:36:26 -0400

    created functions to initialize app

--- server.js
+++ server.js
@@ -3,3 +3,9 @@ const inquirer = require("inquirer");
 
 // gets Specific arrays { } from questions.js
 const {} = require("./lib/questions.js");
+
+// Creates a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
